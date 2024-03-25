977044661e6665e119b237b9a2f3018d733237e0
Martin Adam <martin.viana99@gmail.com>
2024-03-25 16:04:40 -0300

    test

--- islands/Newsletter.tsx
+++ islands/Newsletter.tsx
@@ -2,7 +2,7 @@ import type { Props } from "../components/footer/Newsletter.tsx";
 import Component from "../components/footer/Newsletter.tsx";
 
 function Island(props: Props) {
-  return <Component {...props} />;
+  return <Component {...props} />; 
 }
 
 export default Island;
