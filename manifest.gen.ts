9feaa04f72909d0733d69c1085c114298f485c63
Luciano Santos <luciano.santos@deco.cx>
2024-04-11 09:52:16 +0000

    Release Staging environment with 1 changes made by Luciano Santos

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,35 +2,35 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$0 from "./actions/submitRsvp.ts";
-import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$0 from "./loaders/availableIcons.ts";
-import * as $$$1 from "./loaders/icons.ts";
-import * as $$$$$$0 from "./sections/Animation/Animation.tsx";
-import * as $$$$$$1 from "./sections/DecoDay.tsx";
-import * as $$$$$$2 from "./sections/Gallery.tsx";
-import * as $$$$$$3 from "./sections/SocialLinks.tsx";
-import * as $$$$$$4 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$$$$$0 from "./actions/submitRsvp.ts";
+import * as $$$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$0 from "./loaders/availableIcons.ts";
+import * as $$$$1 from "./loaders/icons.ts";
+import * as $$$$$$$0 from "./sections/Animation/Animation.tsx";
+import * as $$$$$$$1 from "./sections/DecoDay.tsx";
+import * as $$$$$$$2 from "./sections/Gallery.tsx";
+import * as $$$$$$$3 from "./sections/SocialLinks.tsx";
+import * as $$$$$$$4 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
-    "deco-sites/girls-have-k8s/loaders/availableIcons.ts": $$$0,
-    "deco-sites/girls-have-k8s/loaders/icons.ts": $$$1,
+    "deco-sites/girls-have-k8s/loaders/availableIcons.ts": $$$$0,
+    "deco-sites/girls-have-k8s/loaders/icons.ts": $$$$1,
   },
   "sections": {
-    "deco-sites/girls-have-k8s/sections/Animation/Animation.tsx": $$$$$$0,
-    "deco-sites/girls-have-k8s/sections/DecoDay.tsx": $$$$$$1,
-    "deco-sites/girls-have-k8s/sections/Gallery.tsx": $$$$$$2,
-    "deco-sites/girls-have-k8s/sections/SocialLinks.tsx": $$$$$$3,
-    "deco-sites/girls-have-k8s/sections/Theme/Theme.tsx": $$$$$$4,
+    "deco-sites/girls-have-k8s/sections/Animation/Animation.tsx": $$$$$$$0,
+    "deco-sites/girls-have-k8s/sections/DecoDay.tsx": $$$$$$$1,
+    "deco-sites/girls-have-k8s/sections/Gallery.tsx": $$$$$$$2,
+    "deco-sites/girls-have-k8s/sections/SocialLinks.tsx": $$$$$$$3,
+    "deco-sites/girls-have-k8s/sections/Theme/Theme.tsx": $$$$$$$4,
   },
   "actions": {
-    "deco-sites/girls-have-k8s/actions/submitRsvp.ts": $$$$$$$$$0,
+    "deco-sites/girls-have-k8s/actions/submitRsvp.ts": $$$$$$$$$$0,
   },
   "apps": {
-    "deco-sites/girls-have-k8s/apps/decohub.ts": $$$$$$$$$$$0,
-    "deco-sites/girls-have-k8s/apps/site.ts": $$$$$$$$$$$1,
+    "deco-sites/girls-have-k8s/apps/decohub.ts": $$$$$$$$$$$$0,
+    "deco-sites/girls-have-k8s/apps/site.ts": $$$$$$$$$$$$1,
   },
   "name": "deco-sites/girls-have-k8s",
   "baseUrl": import.meta.url,
